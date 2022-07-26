df480495242fa90050c103848da7401ae5604891
Fabio <fabio@kenzie.com.br>
2022-07-26 10:48:24 -0300

    Sprint 1 dia 2

--- src/app.js
+++ src/app.js
@@ -1,23 +1,38 @@
 import express from "express"
+import { v4 as uuidv4 } from "uuid"
 
 const app = express()
 app.use(express.json())
 
-app.get('/users', (request, response) => {
-    return response.json([
-        {
-            nome: 'FAbio'
-        },
-        {
-            nome: 'Felipe'
-        }
-    ])
-})
+const users = []
+
+const userExists = (request, response, next) => {
+    const { id } = request.params
+    const user = users.find(user => user.id === id)
+
+    if(user){
+        request.user = user
+        next()
+    }
+
+    return response.status(404).json({
+        message: 'User not found'
+    })
+}
 
 app.post('/users', (request, response) => {
     const user = request.body
-    console.log(user)
-    return res.send()
+    user.id = uuidv4()
+    users.push(user)
+    return response.status(201).json(user)
+})
+
+app.get('/users', (request, response) => {
+    return response.json(users)
+})
+
+app.get('/users/:id', userExists, (request, response) => {
+    return response.json(request.user)
 })
 
 app.listen(3000, () => {
