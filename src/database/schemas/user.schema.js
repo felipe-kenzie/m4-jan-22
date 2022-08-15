c2cfaf908533582c6d4006e19421a042c47d4f16
Fabio <fabio@kenzie.com.br>
2022-08-15 09:35:14 -0300

    Delete files

--- src/database/schemas/user.schema.js
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup"
-
-const userSchema = yup.object().shape({
-    name: yup.string().required(),
-    email: yup.string().email().required()
-})
-
-export default userSchema
